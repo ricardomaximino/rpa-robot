00499a562301f39f5cea622a301e434dab74a0a5
Ricardo Maximino <ricardomaximino@hotmail.com>
2021-08-12 17:32:05 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from RPA.Browser.Selenium import Selenium
+from RPA.FileSystem import FileSystem
+import time
+browser = Selenium()
+file_system = FileSystem()
+url = "https://robotframework.org/"
+
+
+def store_web_page_content():
+    browser.open_available_browser(url)
+    browser.maximize_browser_window()
+    time.sleep(4)
+    text = browser.get_text("scroller")
+    file_system.create_file("output/text.txt", text, overwrite=True)
+    browser.screenshot("css:.img-fluid", "output/screenshot.png")
+
+
+def main():
+    try:
+        store_web_page_content()
+    finally:
+        browser.close_browser()
+
+
+if __name__ == "__main__":
+    main()
